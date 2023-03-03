3c1fb5ddc3babd82ec5ad9f026057932c3984b5d
HardCPP <hardcpp@gmail.com>
2023-03-03 03:02:19 +0100

    V5.0.6

---
+++ BeatSaberPlus/CP_SDK/Network/LiteNetLib/Utils/INetSerializable.cs
@@ -0,0 +1,8 @@
+﻿namespace CP_SDK_LiteNetLib.Utils
+{
+    public interface INetSerializable
+    {
+        void Serialize(NetDataWriter writer);
+        void Deserialize(NetDataReader reader);
+    }
+}
